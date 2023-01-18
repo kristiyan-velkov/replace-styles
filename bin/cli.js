1b653860addfb7ee2413daf113d960900ded8f5c
Kristiyan Velkov <kristiyan.velkov@gfk.com>
2023-01-18 14:28:39 +0200

    [feat] update logic for replace

--- bin/cli.js
+++ bin/cli.js
@@ -4,40 +4,40 @@
 import chalk from "chalk";
 import glob from "glob";
 import fs from "fs";
+import { combineMatchers } from "../lib/helpers/combine-matchers.js";
 
 // Glob over the files
 
-const replaceCssSelectors = async (file, data, toReplace, selectors) => {
-  selectors.forEach((selector) => {
-    const matcher = `${selector}${toReplace.from};`;
-    if (data.includes(matcher)) {
-      // const regex = new RegExp(`\b(\:${matcher}\)b`, 'gmi');
-      const regex = new RegExp(`${matcher}`, "gmi");
-      const replacedResult = data.replace(regex, `${selector}${toReplace.to};`);
+const writeInFiles = async (file, data, matchers) => {
+  const writeData = (replacedData) => {
+    console.log(replacedData);
+    fs.writeFile(file, replacedData, "utf8", function (err) {
+      if (err) throw err;
+      console.log(chalk.cyan("File was updated:", file));
+    });
+  };
 
-      fs.writeFile(file, replacedResult, "utf8", function (err) {
-        if (err) throw err;
-        console.log(chalk.cyan("File was updated:", file));
-      });
-    } else {
-      console.log(chalk.red("No matches!"));
-    }
+  matchers.forEach((item) => {
+    const regex = new RegExp(item.from, "gmi");
+    data = data.replace(regex, item.to);
   });
+
+  writeData(data);
 };
 
-const writeInfile = (pathToFiles, encoding, replace, selectors) => {
-  glob.sync(pathToFiles, { nodir: true }).forEach((file) => {
+const readFiles = (paths, encoding, replace, selectors) => {
+  const matchers = combineMatchers(replace, selectors);
+
+  glob.sync(paths, { nodir: true }).forEach((file) => {
     fs.readFile(file, encoding, (err, data) => {
       if (err) throw err;
-      replace.forEach((toReplace) => {
-        replaceCssSelectors(file, data, toReplace, selectors);
-      });
+      writeInFiles(file, data, matchers);
     });
   });
 };
 
 const scanForFiles = (config) => {
-  const { pathToFiles, encoding = "utf8", replace, selectors } = config;
+  const { paths, encoding = "utf8", replace, selectors } = config;
 
   const callbackFunc = (error, files) => {
     if (error) {
@@ -47,25 +47,29 @@ const scanForFiles = (config) => {
     if (files && files.length > 0) {
       console.log(chalk.yellow.bold(`Found files: ${files.length}`));
       console.table([...files]);
-      writeInfile(pathToFiles, encoding, replace, selectors);
+      readFiles(paths, encoding, replace, selectors);
     } else {
       console.log(chalk.red("No file founds!"));
-      console.log(chalk.cyan("Please, try to change the path to files."));
+      console.log(chalk.cyan("Please, try to change the paths to files."));
     }
   };
 
-  glob(pathToFiles, callbackFunc);
+  glob(paths, callbackFunc);
 };
 
 // Config
 
 const config = {
-  pathToFiles: "src/**/*.{sass,scss,css}",
-  selectors: [": "],
+  paths: "src/*.scss",
+  selectors: [": ", "color: ", "test: "],
   replace: [
     {
       from: "red",
-      to: "blue",
+      to: "orange",
+    },
+    {
+      from: "gold",
+      to: "pink",
     },
   ],
 };
