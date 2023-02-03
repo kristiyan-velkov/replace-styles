029ea0fcf83e3c8bd9458f0f6b9a49b6eebd4b9d
Kristiyan Velkov <kristiyan.velkov@gfk.com>
2023-02-03 16:26:11 +0200

    [fix] Index js paths to utils

--- publish/index.js
+++ publish/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 "use strict";
 import e from "chalk";
-import o from "./utils/config/validate-config.js";
-import i from "./find-style-files.js";
-import r from "./utils/config/find-config.js";
+import o from "./src/utils/config/validate-config.js";
+import i from "./src/find-style-files.js";
+import r, { findConfig } from "./src/utils/config/find-config.js";
 export const replaceStyles = (r) => {
   r && o(r) ? i(r) : console.log(e.red.bold("Please, provide config!"));
 };
